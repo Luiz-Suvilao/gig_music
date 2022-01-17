b25ca56ecfb1dcd0739d2d264acb7e35743e6707
Luiz Filipe <infobase.lfsilva@terceiros.mag.com.br>
2022-01-17 20:18:04 -0300

    Tornando header dinâmico

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,57 @@
+import { GetStaticProps } from 'next';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
+
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
+import { getPrismicClient } from '../../services/prismic';
 
 import ActiveLink from '../ActiveLink';
 
-import logo from '../../../public/images/logo_gig.jpg';
-
 import styles from './styles.module.scss';
 
+type HeaderData = {
+    logo: string;
+    logoAlt: string;
+    link: string;
+    textButton: string;
+}
+
 const Header = () => {
+    const [headerData, setHeaderData] = useState<HeaderData>({} as HeaderData);
+
+    useEffect(() => {
+        (async function load() {
+            const prismic = getPrismicClient();
+            const response = await prismic.query([
+                Prismic.predicates.at('document.type', 'header')
+            ]);
+
+            const {
+                logo_header,
+                link_button,
+                text_button
+            } = response.results[0].data;
+            console.log(response.results[0].data);
+
+            const headerData = {
+                logo: logo_header.url,
+                logoAlt: logo_header.alt,
+                link: link_button.url,
+                textButton: RichText.asText(text_button),
+            };
+
+            setHeaderData(headerData);
+        })();
+    }, []);
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <ActiveLink href="/" activeClassName={styles.active}>
                     <a>
-                        <Image width={80} height={50} src={logo} alt="Logo da gig music"/>
+                        <img width={80} height={50} src={headerData.logo} alt={headerData.logoAlt}/>
                     </a>
                 </ActiveLink>
 
@@ -30,7 +69,7 @@ const Header = () => {
                     </ActiveLink>
                 </nav>
 
-                <a className={styles.readyButton} type="button" href="#">Começar</a>
+                <a rel="noreferrer" target="_blank" className={styles.readyButton} type="button" href={headerData.link}>{headerData.textButton}</a>
             </div>
         </header>
     )

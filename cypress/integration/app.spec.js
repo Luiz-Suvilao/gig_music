f371b728d6cb03b2a7bb76130e2bf64eab8111a0
Luiz Filipe <infobase.lfsilva@terceiros.mag.com.br>
2022-02-07 22:56:08 -0300

    Configurando Cypress

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,7 +1,13 @@
 describe('Navigation to conteúdos', () => {
     it('should navigate to the conteúdos page', () => {
-        cy.visit('http://localhost:3000/')
+        cy.visit('/posts')
         cy.get('a[href*="posts"]').click()
         cy.url().should('include', '/posts')
+    });
+
+    it('should navigate to the sobre page', () => {
+        cy.visit('/posts')
+        cy.get('a[href*="sobre"]').click()
+        cy.url().should('include', '/sobre')
     })
 });
